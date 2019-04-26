9ed5075dc741cc61a57a3003d9554ece0b34dd23
Marco Cutecchia <marco.cutecchia@outlook.it>
2019-04-26 20:09:55 +0200

    added keyboard support

--- src/components/GameInput.js
+++ src/components/GameInput.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 
 
 class GameInput extends Component{
+    constructor(props){
+        super(props);
+        document.addEventListener("keydown", (e) => {
+            const cb = this.props.onInput;
+            switch(e.key){
+                case "ArrowLeft":
+                    cb("move", "left");
+                    break;
+                case "ArrowRight":
+                    cb("move", "right");
+                    break;
+                case "a":
+                case "A":
+                    cb("rotate", "left");
+                    break;
+                case "s":
+                case "S":
+                    cb("rotate", "right");
+                    break;
+                default:
+            }
+        });
+    }
     render(){
         const cb = this.props.onInput;
         return (
