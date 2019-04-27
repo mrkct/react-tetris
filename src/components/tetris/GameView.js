db17fe9c3cd2113129592178da0c52d7103f1b4c
Marco Cutecchia <marco.cutecchia@outlook.it>
2019-04-27 11:40:17 +0200

    added background + nicer blocks

--- src/components/tetris/GameView.js
+++ src/components/tetris/GameView.js
@@ -1,5 +1,30 @@
 import React, { Component } from 'react';
 
+/**
+ * Takes a hex string representing a color and lighten it. Positive 
+ * values make the color brighter, negative make it darker.
+ * From: https://www.sitepoint.com/javascript-generate-lighter-darker-color/
+ * @param {String} hex: Hex string of a color 
+ * @param {Number} lum: How much to brighten, or darken, the color 
+ */
+function ColorLuminance(hex, lum) {
+    // validate hex string
+    hex = String(hex).replace(/[^0-9a-f]/gi, '');
+    if (hex.length < 6) {
+        hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+    }
+    lum = lum || 0;
+
+    // convert to decimal and change luminosity
+    var rgb = "#", c, i;
+    for (i = 0; i < 3; i++) {
+        c = parseInt(hex.substr(i*2,2), 16);
+        c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+        rgb += ("00"+c).substr(c.length);
+    }
+
+    return rgb;
+}
 
 /**
  * Component that shows an array of arrays in a canvas, where each element
@@ -21,7 +46,7 @@ class GameView extends Component {
     }
 
     drawBlock(context, x, y, blockSize, color){
-        const thickness = 2;
+        const thickness = 1;
         context.fillStyle = "black";
         context.fillRect(
             blockSize * x,
@@ -36,6 +61,27 @@ class GameView extends Component {
             blockSize - (thickness*2),
             blockSize - (thickness*2)
         );
+        context.fillStyle = ColorLuminance(color, -0.2);
+        context.fillRect(
+            (blockSize * x) + blockSize/4,
+            (blockSize * y) + blockSize/4,
+            blockSize / 2,
+            blockSize / 2
+        )
+    }
+
+    drawBackground(context, width, height, blockSize){
+        context.fillStyle = "black";
+        context.fillRect(
+            0, 0, blockSize * width, blockSize * height
+        );
+        context.fillStyle = "gray";
+        for(let i = 0; i < width; i++){
+            context.fillRect(0, i * blockSize, width * blockSize, 1);
+        }
+        for(let i = 0; i < height; i++){
+            context.fillRect(i * blockSize, 0, 1, height * blockSize);
+        }
     }
 
     componentDidUpdate(){
@@ -46,8 +92,7 @@ class GameView extends Component {
         const height = this.height;
 
         ctx.save();
-        ctx.fillStyle = "gray";
-        ctx.fillRect(0, 0, width, height);
+        this.drawBackground(ctx, width, height, blockSize);
         
         const board = this.props.board;
         for(let i = 0; i < board.length; i++) {
