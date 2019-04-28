0f49f7d1882d5b88ac23852016a6728b6f7b8348
Marco Cutecchia <marco.cutecchia@outlook.it>
2019-04-28 19:40:39 +0200

    bigger buttons for mobile

--- src/components/tetris/GameInput.js
+++ src/components/tetris/GameInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './GameInput.css';
 
 
 export class GameInput extends Component{
@@ -32,21 +33,25 @@ export class GameInput extends Component{
         const cb = this.props.onInput;
         return (
             <div className="gameController">
-                <button onClick={cb.bind(this, Input.MOVE.LEFT)}>
-                    Left
-                </button>
-                <button onClick={cb.bind(this, Input.MOVE.RIGHT)}>
-                    Right
-                </button>
-                <button onClick={cb.bind(this, Input.MOVE.DOWN)}>
-                    Down
-                </button>
-                <button onClick={cb.bind(this, Input.ROTATE.LEFT)}>
-                    Rotate Left
-                </button>
-                <button onClick={cb.bind(this, Input.ROTATE.RIGHT)}>
-                    Rotate Right
-                </button>
+                <div className="moveButtons">
+                    <button className="gameButton" onClick={cb.bind(this, Input.MOVE.LEFT)}>
+                        Left
+                    </button>
+                    <button className="gameButton" onClick={cb.bind(this, Input.MOVE.DOWN)}>
+                        Down
+                    </button>
+                    <button className="gameButton" onClick={cb.bind(this, Input.MOVE.RIGHT)}>
+                        Right
+                    </button>
+                </div>
+                <div className="rotateButtons">
+                    <button className="gameButton" onClick={cb.bind(this, Input.ROTATE.LEFT)}>
+                        Rotate Left
+                    </button>
+                    <button className="gameButton" onClick={cb.bind(this, Input.ROTATE.RIGHT)}>
+                        Rotate Right
+                    </button>
+                </div>
             </div>
         );
     }
